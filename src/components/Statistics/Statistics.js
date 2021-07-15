bee63eb6f2b184e137ca3ef2cf5631f2887cf797
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-07-15 23:45:45 +0300

    gh-pages add

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,8 +2,8 @@ import React from 'react'
 import style from './Statistics.module.css'
 const Statistics = ({good,neutral,bad,total,positiveFeedbackPercentage}) => (
         <ul>
-            <li><p>Good :{good }</p></li>
-            <li><p>Neutral :{neutral}</p></li>
+            <li><p>Good : {good }</p></li>
+            <li><p>Neutral : {neutral}</p></li>
             <li><p>Bad : {bad}</p></li>
             <li><p>Total :{total}</p></li>
             <li><p>Positive feedback : {typeof positiveFeedbackPercentage ==='number'&&!positiveFeedbackPercentage?' No reviews':positiveFeedbackPercentage+'%'}</p></li>
